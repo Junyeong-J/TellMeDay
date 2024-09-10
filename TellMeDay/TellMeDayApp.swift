67f420faad72e7554349ba0ccee97e9dcb3f2c8e
JunYeong-J <eeot2827@gmail.com>
2024-09-10 16:12:11 +0900

    Initial Commit

---
+++ TellMeDay/TellMeDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellMeDayApp.swift
+//  TellMeDay
+//
+//  Created by 전준영 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TellMeDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
