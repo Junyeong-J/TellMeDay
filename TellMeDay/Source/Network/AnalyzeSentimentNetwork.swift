d8b52ada1933a20d8641d0efb56575f52bdca8ff
JunYeong-J <eeot2827@gmail.com>
2024-09-28 00:49:59 +0900

    Feat: ginignore설정

---
+++ TellMeDay/Source/Network/AnalyzeSentimentNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  analyzeSentimentNetwork.swift
+//  TellMeDay
+//
+//  Created by 전준영 on 9/28/24.
+//
+
+import Foundation
---
+++ TellMeDay/Source/Views/ResultView/AnalysisResultView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalysisResultView.swift
+//  TellMeDay
+//
+//  Created by 전준영 on 9/27/24.
+//
+
+import SwiftUI
+
+struct AnalysisResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalysisResultView()
+}
