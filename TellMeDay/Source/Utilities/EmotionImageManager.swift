30370b9d0297fed79a9379a87466c24598888136
JunYeong-J <eeot2827@gmail.com>
2024-09-28 23:41:53 +0900

    Faet: 감정 분석 이미지

--- TellMeDay/Source/Utilities/EmotionImageManager.swift
+++ TellMeDay/Source/Utilities/EmotionImageManager.swift
@@ -5,4 +5,21 @@
 //  Created by 전준영 on 9/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct EmotionImageManager {
+    static func image(for emotion: String) -> Image {
+        switch emotion {
+        case "기쁨":
+            return Image("happyEmoji")
+        case "슬픔":
+            return Image(systemName: "moon.fill")
+        case "공포":
+            return Image(systemName: "exclamationmark.triangle.fill")
+        case "분노":
+            return Image(systemName: "flame.fill")
+        default:
+            return Image(systemName: "questionmark.circle.fill")
+        }
+    }
+}
