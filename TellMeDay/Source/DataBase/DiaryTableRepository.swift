678fa745838bb1cedd0c3b17b5353fd489998b63
JunYeong-J <eeot2827@gmail.com>
2024-09-29 23:45:41 +0900

    Feat: Realm설정

--- TellMeDay/Source/DataBase/DiaryTableRepository.swift
+++ TellMeDay/Source/DataBase/DiaryTableRepository.swift
@@ -6,3 +6,116 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class ListTableRepository {
+    
+    private let realm = try! Realm()
+    
+    func detectRealmURL() {
+        print(realm.configuration.fileURL ?? "")
+    }
+    
+    func updateCategory(_ category: String, for entry: DiaryEntry) {
+        try! realm.write {
+            if let parentFolder = entry.parentFolder.first {
+                parentFolder.category = category
+            }
+        }
+    }
+    
+    func updateEntry(_ entry: DiaryEntry, newTitle: String, newCategory: String) {
+        try! realm.write {
+            entry.title = newTitle
+            if let parentFolder = entry.parentFolder.first {
+                parentFolder.category = newCategory
+            }
+        }
+    }
+}
+
+//MARK: - 저장
+extension ListTableRepository {
+    func saveFolder(category: String) -> DiaryFolder {
+        let folder = DiaryFolder(category: category)
+        try! realm.write {
+            realm.add(folder)
+        }
+        return folder
+    }
+    
+    func saveEntry(to folder: DiaryFolder, title: String, content: String?, selectedDate: Date,
+                   audioFilePath: String?, positiveScore: Int?, negativeScore: Int?, emotion: String?) {
+        let entry = DiaryEntry(
+            title: title,
+            content: content,
+            selectedDate: selectedDate,
+            audioFilePath: audioFilePath,
+            positiveScore: positiveScore,
+            negativeScore: negativeScore,
+            emotion: emotion
+        )
+        try! realm.write {
+            folder.entries.append(entry)
+        }
+    }
+}
+
+//MARK: - 가져오기
+extension ListTableRepository {
+    
+    func fetchEntry(by id: ObjectId) -> DiaryEntry? {
+        return realm.objects(DiaryEntry.self).filter("id == %@", id).first
+    }
+    
+    func fetchFolder() -> [DiaryFolder] {
+        let value = realm.objects(DiaryFolder.self)
+        return Array(value)
+    }
+    
+    func fetchEntry(for date: Date) -> DiaryEntry? {
+        let startOfDay = Calendar.current.startOfDay(for: date)
+        let endOfDay = Calendar.current.date(byAdding: .day, value: 1, to: startOfDay)!
+        
+        return realm.objects(DiaryEntry.self)
+            .filter("selectedDate >= %@ AND selectedDate < %@", startOfDay, endOfDay)
+            .first
+    }
+    
+    func fetchEntryTo(for date: Date) -> DiaryEntry? {
+        let entries = realm.objects(DiaryEntry.self).filter("selectedDate == %@", date)
+        return entries.first
+    }
+    
+    func fetchEntries(for year: Int, month: Int) -> [DiaryEntry] {
+        let calendar = Calendar.current
+        
+        var startComponents = DateComponents(year: year, month: month, day: 1)
+        let startDate = calendar.date(from: startComponents)!
+        
+        startComponents.month = month + 1
+        let endDate = calendar.date(from: startComponents)!
+        
+        let entries = realm.objects(DiaryEntry.self)
+            .filter("selectedDate >= %@ AND selectedDate < %@", startDate, endDate)
+        
+        return Array(entries)
+    }
+    
+}
+
+//MARK: - 삭제
+extension ListTableRepository {
+    func deleteEntryAndFolderIfNeeded(_ entry: DiaryEntry) {
+        try! realm.write {
+            if let folder = entry.parentFolder.first {
+                folder.entries.remove(at: folder.entries.index(of: entry)!)
+                realm.delete(entry)
+                if folder.entries.isEmpty {
+                    realm.delete(folder)
+                    print("DiaryFolder 삭제 완료")
+                }
+            }
+        }
+    }
+}
--- TellMeDay/Source/DataBase/RealmModel.swift
+++ TellMeDay/Source/DataBase/RealmModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import RealmSwift
+
+class DiaryFolder: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var category: String
+    @Persisted var createdDate: Date
+    @Persisted var entries: List<DiaryEntry>
+    
+    convenience init(category: String) {
+        self.init()
+        self.category = category
+        self.createdDate = Date()
+    }
+}
+
+class DiaryEntry: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var content: String?
+    @Persisted var selectedDate: Date
+    @Persisted var audioFilePath: String?
+    @Persisted var positiveScore: Int?
+    @Persisted var negativeScore: Int?
+    @Persisted var emotion: String?
+
+    @Persisted(originProperty: "entries") var parentFolder: LinkingObjects<DiaryFolder>
+    
+    convenience init(title: String, content: String?, selectedDate: Date,
+                     audioFilePath: String?,positiveScore: Int?,
+                     negativeScore: Int?, emotion: String?) {
+        self.init()
+        self.title = title
+        self.content = content
+        self.selectedDate = selectedDate
+        self.audioFilePath = audioFilePath
+        self.positiveScore = positiveScore
+        self.negativeScore = negativeScore
+        self.emotion = emotion
+    }
+}
